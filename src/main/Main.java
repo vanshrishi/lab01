ae0dcb21feaeb2869f61c056742042420bb6084b
vanshrishi <vansh.rishi_cs18@gla.ac.in>
2019-08-12 02:53:05 -0700

    Main.java: add statement for printing table of a number.

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,10 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World !!!");
+        int a = 2;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(a + " * " + i + " = " + a * i);
+        }
     }
 }
 
